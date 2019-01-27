cb24605375df7ca2c3777babb7a4af49db33429e
taehoon7 <tony.lee0754@gmail.com>
2019-01-26 22:38:40 -0800

    aa

---
+++ TBS_Game_Unity/Assets/Scripts/MoveableArea.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableArea : MonoBehaviour
+{
+    [SerializeField]
+    private UnitData unitData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //sets tiles around unit to lit up 
+    public void getMoveableCells(int pos, int mov)
+    {
+        for (int i = -unitData.Mov; i <= unitData.Mov; i++)
+        {
+            for (int j = -unitData.Mov; j <= unitData.Mov; j++)
+            {
+                if (transform.position.x + i < Constants.BoardSize / 2 && transform.position.x + i > -Constants.BoardSize / 2
+                    && transform.position.y + i < Constants.BoardSize / 2 && transform.position.y + i > -Constants.BoardSize / 2)
+                {
+                    if (!(i == 0 && j == 0))
+                    {
+                        //Vector3Int Pos = Tilemap.WorldtoCell(transform.position);
+                        Click = true;
+                        //Pos.x += i;
+                        //Pos.y += j;
+                        //Tilemap.SetTile(Pos, );
+                    }
+                }
+            }
+        }
+    }
+    public void clearMoveableCells() {
+
+    }
+}
--- TBS_Game_Unity/Assets/Scripts/Unit.cs
+++ TBS_Game_Unity/Assets/Scripts/Unit.cs
@@ -2,18 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+static class Constants
+{
+    public const int BoardSize = 100;
+}
+
 public class Unit : MonoBehaviour
 {
     [SerializeField]
     private UnitData unitData;
+    private bool Hover;
 
-    // Update is called once per frame
-    private void OnMouseDown()
+    //display health, attack, special
+    private void OnGUI()
+    {
+        if (Hover)
+        {
+            GUI.Label(new Rect(10, 10, 100, 20), "HP: "+ unitData.HP.ToString());
+            GUI.Label(new Rect(10, 30, 100, 20), "Attack: " + unitData.Attack.ToString());
+            GUI.Label(new Rect(10, 50, 100, 20), "Special: " + unitData.Special.ToString());
+        }
+    }
+    private void OnMouseOver()
     {
         Debug.Log(unitData.Name);
         Debug.Log(unitData.HP);
         Debug.Log(unitData.Attack);
         Debug.Log(unitData.Mov);
         Debug.Log(unitData.Special);
+        Hover = true;
+
+    }      
+
+    private void OnMouseExit()
+    {
+        Hover = false;
     }
+
+    UnityEngine.AI.NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+
+
 }

b6dabd0b6807d2a168e767e6a783003603a1d2a9
apollo78124 <98apollo@naver.com>
2019-01-27 00:44:26 -0800

    login page 2

---
+++ TBS_Game_Unity/Assets/Scripts/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class Login : MonoBehaviour
+{
+    public InputField idField;
+    public InputField pwField;
+
+    public void Start()
+    {
+        idField.text = "Enter Your ID Here...";
+        pwField.text = "Enter Your Password Here...";
+    }
+}

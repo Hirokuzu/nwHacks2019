e1eefb728fbdff93a8cecbd7ffd064d7b2730873
taehoon7 <tony.lee0754@gmail.com>
2019-01-26 17:26:26 -0800

    ScriptableObjects Done

---
+++ TBS_Game_Unity/Assets/Scripts/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField]
+    private UnitData unitData;
+
+    // Update is called once per frame
+    private void OnMouseDown()
+    {
+        Debug.Log(unitData.Name);
+        Debug.Log(unitData.HP);
+        Debug.Log(unitData.Attack);
+        Debug.Log(unitData.Mov);
+        Debug.Log(unitData.Special);
+    }
+}
---
+++ TBS_Game_Unity/Assets/Scripts/UnitData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Unit", menuName = "Unit", order = 51)]
+public class UnitData : ScriptableObject
+{
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private int hp;
+    [SerializeField]
+    private int attack;
+    [SerializeField]
+    private int mov;
+    [SerializeField]
+    private string special;
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public int HP
+    {
+        get
+        {
+            return hp;
+        }
+    }
+
+    public int Attack
+    {
+        get
+        {
+            return attack;
+        }
+    }
+
+    public int Mov
+    {
+        get
+        {
+            return mov;
+        }
+    }
+
+    public string Special
+    {
+        get
+        {
+            return special;
+        }
+    }
+
+}
